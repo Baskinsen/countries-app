0939b48a0009c126e8cfdccec82923057303cd60
Baskinsen <92514412+Baskinsen@users.noreply.github.com>
2024-02-06 16:36:52 +0100

    codebase init

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1>Where in the world?</h1>
+    <div>
+        
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-blue-700">home</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
